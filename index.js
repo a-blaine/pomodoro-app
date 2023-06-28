8c1e0885ef883054dc1e69ac6b6301e56b9483e8
a-blaine <ai.blaine4@protonmail.com>
2023-06-28 16:09:13 +0100

    Added countdown functionality

--- index.js
+++ index.js
@@ -2,12 +2,33 @@ let workButton = document.querySelector("#work");
 let breakButton = document.querySelector("#break");
 let playButton = document.querySelector("#play");
 let minutesElement = document.querySelector("#minutes");
+let secondsElement = document.querySelector("#seconds");
 
-let seconds = "00";
-let milliseconds = "00";
+let workTime = 50;
+let breakTime = 10;
 
 function start(event) {
   event.preventDefault();
+  seconds = 59;
+  let workCountdown = workTime - 1;
+  let breakCountdown = breakTime - 1;
+
+  const countdown = () => {
+    minutesElement.innerHTML = workCountdown;
+    secondsElement.innerHTML = seconds;
+    seconds = seconds - 1;
+
+    if (seconds < 10) {
+      seconds = 0 + `${seconds}`;
+    }
+
+    if (seconds == 0) {
+      seconds = 59;
+      workCountdown = workCountdown - 1;
+    }
+  };
+
+  setInterval(countdown, 1000);
 }
 
 function setTimer(time) {
@@ -21,12 +42,15 @@ function setTimer(time) {
   }
 }
 
-playButton.addEventListener("click", start);
+playButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  start();
+});
 breakButton.addEventListener("click", (event) => {
   event.preventDefault();
-  setTimer(10);
+  setTimer(breakTime);
 });
 workButton.addEventListener("click", (event) => {
   event.preventDefault();
-  setTimer(50);
+  setTimer(workTime);
 });
